b8470894d06af24583e08fc7594dc7204cbd4c8b
stc00012 <165570384+stc00012@users.noreply.github.com>
2024-10-16 13:49:17 +0200

    fix_gates_of_hell

--- Gates_of_hell.py
+++ Gates_of_hell.py
@@ -1,11 +1,18 @@
 def door():
-    print("You've just entered in the castle!")
-    print("Do you take the door on the left or the right?")
+    print("The castle is collapsing, ruuun!")
+    print("ops There are two doors, Do you take the door on the left or the right?")
     answer = input(("Type left or right and hit 'Enter'.").lower())
     if answer == "left" or answer == "l":
-        print("This is the Room of the hell, you just died!")
+        print("This is the Room of the dragon!!!!, you just died!  :( ")
     elif answer == "right" or answer == "r":
-        print("It is the exit, you are lucky!")
+        print("Great, but almost done, there another two door!")
+        answer = input(("Type left or right and hit 'Enter'.").lower())
+        if answer == "left" or answer == "l":
+            print("It is the exit, you are lucky!")
+        elif answer == "right" or answer == "r":
+            print("Oh noo, the giant two headed dog, you died!!! :( ")
+        else:
+            print("Too slow, you die!!!! ")
     else:
         print("You didn't pick left or right! Try again.")
         door()
